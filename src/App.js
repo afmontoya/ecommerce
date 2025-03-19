c2e5faa7277fd33a43fec992ee146ce8b57ae927
Albert F. Montoya Jr. <albert@montoyamedia.com>
2025-03-19 18:22:17 +0000

    Add Creative Marketplace platform with functional UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreativeMarketplace from './CreativeMarketplace';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CreativeMarketplace />
+    </div>
+  );
+}
+
+export default App;
